92b24e8d40d5cc0b3a66e85061f62042d0d3e6e7
Tia Piol <tiarox02@yahoo.com>
2025-07-15 15:01:46 -0700

    updated exercise 2

--- server.js
+++ server.js
@@ -13,23 +13,24 @@ app.get('/greetings/:username', (req, res) => {
 
 app.get('/roll/:number', (req, res) => {
 
-    if (!Number(req.params.number)) {
+    if (isNaN(req.params.number)) {
         return res.send(`<h1>you must specify a number</h1>`);
     }
-    const roll = Math.floor(Math.random() * (req.params.number) + 1);
+    const roll = Math.floor(Math.random() * (req.params.number));
     res.send(`You rolled a ${roll}`)
 });
 
 // 3. I Want THAT One!
 // Task: Create a route for URLs like /collectibles/<index-parameter>.
- 
-app.get('/collectibles/:index', (req, res) => {
 
- const collectibles = [
+const collectibles = [
     { name: 'shiny ball', price: 5.95 },
     { name: 'autographed picture of a dog', price: 10 },
     { name: 'vintage 1970s yogurt SOLD AS-IS', price: 0.99 }
-  ];
+  ]; 
+
+app.get('/collectibles/:index', (req, res) => {
+
 
   const index = Number(req.params.index);
 
@@ -45,9 +46,7 @@ const item = collectibles[index]
 
 // 4. Filter Shoes by Query Parameters
 // Task: Create a route /shoes that filters the list of shoes based on query parameters.
-
-app.get('/shoes', (req, res) => {
-      const shoes = [
+     const shoes = [
       { name: "Birkenstocks", price: 50, type: "sandal" },
       { name: "Air Jordans", price: 500, type: "sneaker" },
       { name: "Air Mahomeses", price: 501, type: "sneaker" },
@@ -56,6 +55,8 @@ app.get('/shoes', (req, res) => {
       { name: "Jet Boots", price: 1000, type: "boot" },
       { name: "Fifty-Inch Heels", price: 175, type: "heel" }
   ];
+
+app.get('/shoes', (req, res) => {
 const minPrice = Number(req.query.minPrice);
 const maxPrice = Number(req.query.maxPrice);
 const type = req.query.type;
